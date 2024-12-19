aba42d236364923251b5d16c9d061d0874faddd6
gabriel-qs <gabriel.queiroz9543@gmail.com>
2024-12-19 14:01:37 -0300

    Crie var scores e a funcao getComputerChoice()

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //Arquivo JS do jogo da velha
+var humanScore = 0;
+var computerScore = 0;
 
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+
+function getComputerChoice() {
+	let drawNumber = Math.floor(Math.random() * (4 - 1) + 1); //Gera um num aleatorio entre 1 a 3
+	
+	if(drawNumber === 1) {
+		return "rock";
+	} 
+	else if(drawNumber === 2) {
+		return "paper";
+	} 
+	else {
+		return "scissors";
+	}
+	
+} 
